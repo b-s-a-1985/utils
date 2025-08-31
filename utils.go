1f1fc4302b0b5d1c694ce908015c4639076cf67a
Bodnár Sándor <bodnarsa@gmail.com>
2025-08-31 07:27:40 +0200

    Update utils.go

--- utils.go
+++ utils.go
@@ -150,7 +150,7 @@ func ReverseString(s string) string {
 var(
 	FgBlack="\x1b[30m"
 	FgRed="\x1b[31m"
-	FgGreen"\x1b[32m"
+	FgGreen="\x1b[32m"
 	FgYellow="\x1b[33m"
 	FgBlue="\x1b[34m"
 	FgMagenta="\x1b[35m"
