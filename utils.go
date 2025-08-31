ba05c2c27f731bef2e6547300469006c244d21a5
Bodnár Sándor <bodnarsa@gmail.com>
2025-08-31 07:13:27 +0200

    Update utils.go

--- utils.go
+++ utils.go
@@ -145,3 +145,16 @@ func ReverseString(s string) string {
 	}
 	return string(runes)
 } // ReverseString
+
+// Foreground colors
+var(
+	FgBlack="\x1b[30m"
+	FgRed="\x1b[31m"
+	FgGreen"\x1b[32m"
+	FgYellow="\x1b[33m"
+	FgBlue="\x1b[34m"
+	FgMagenta="\x1b[35m"
+	FgCyan="\x1b[36m"
+	FgWhite="\x1b[37m"
+	Reset="\x1b[0m"); // reset
+)
